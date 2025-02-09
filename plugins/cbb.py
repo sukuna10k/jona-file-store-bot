ee36f4c0d3dfb2e9e327464e75105284dc421322
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-09 16:50:23 +0100

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -15,7 +15,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-                        InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')
+                        InlineKeyboardButton("Retour", callback_data='close')
                     ]
                 ]
             )
